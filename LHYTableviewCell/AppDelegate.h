892c5db7d311e780623c6da9a727ace0dcc840e3
lhyIOSer <421790782@qq.com>
2016-05-30 15:25:34 +0800

    Initial Commit

---
+++ LHYTableviewCell/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LHYTableviewCell
+//
+//  Created by lhy on 16/5/30.
+//  Copyright © 2016年 lhy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LHYTableviewCell/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LHYTableviewCell
+//
+//  Created by lhy on 16/5/30.
+//  Copyright © 2016年 lhy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
